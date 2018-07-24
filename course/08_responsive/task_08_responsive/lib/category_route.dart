3beb979e63934c98c6bc3453cdf766da614c760a
GianmarcoVenturini1993 <gianmarco.venturini@outlook.it>
2018-07-24 16:59:13 +0200

    fixed ad error in build method in category_route due to no args in _buildCategoryWidgets

--- course/08_responsive/task_08_responsive/lib/category_route.dart
+++ course/08_responsive/task_08_responsive/lib/category_route.dart
@@ -147,7 +147,7 @@ class _CategoryRouteState extends State<CategoryRoute> {
         right: 8.0,
         bottom: 48.0,
       ),
-      child: _buildCategoryWidgets(),
+      child: _buildCategoryWidgets(MediaQuery.of(context).orientation),
     );
 
     return Backdrop(
